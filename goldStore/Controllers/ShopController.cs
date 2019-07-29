0325e7c44ee79ffd5db88816c30b3f4c3734a810
Muhammet Ucar <mhmucar@hotmail.com>
2019-07-29 15:03:29 +0300

    Shop Controller

---
+++ goldStore/Controllers/ShopController.cs
@@ -0,0 +1,36 @@
+﻿using goldStore.Areas.Panel.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace goldStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        ProductRepository repoProduct = new ProductRepository(new Areas.Panel.Models.goldstoreEntities());
+        CategoryRepository repoCategory = new CategoryRepository(new Areas.Panel.Models.goldstoreEntities());
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return RedirectToAction("Products");
+        }
+        public PartialViewResult PartialCategory()
+        {
+            return PartialView(repoCategory.GetAll());
+        }
+        // ürünleri gösteren method
+        public ActionResult Products(int?categoryId)
+        {
+            var result = repoProduct.GetAll();
+            // eğer kategoriye göre bir filtreleme istendiğinde
+            if (categoryId!=null)
+            {
+                result = result.Where(x => x.categoryId == categoryId).ToList();
+            }
+
+            return View(result);
+        }
+    }
+}
