6f4e3518193c3591dc4b97739cfb09c6b7ebd275
Muhammet Ucar <mhmucar@hotmail.com>
2019-07-31 15:25:08 +0300

    Login

--- goldStore/Controllers/UserController.cs
+++ goldStore/Controllers/UserController.cs
@@ -11,6 +11,7 @@ using System.Net.Configuration;
 using System.Configuration;
 using System.Net.Mail;
 using System.Net;
+using System.Web.Security;
 
 namespace goldStore.Controllers
 {
@@ -91,6 +92,115 @@ namespace goldStore.Controllers
         {
             var user = repoUser.GetAll().Where(a => a.email == username).FirstOrDefault();
             return user == null ? false : true;
+        }
+        //mail adresinizdeki doğrulmayı yapmak için
+        [HttpGet]
+        public ActionResult VerifyAccount(string id)
+        {
+            bool status = false;
+            //db.Configuration.ValidateOnSaveEnabled = false;
+            var result = repoUser.GetAll().Where(a => a.activationCode == new Guid(id).ToString()).FirstOrDefault();
+            if (result != null)
+            {
+                result.isMailVerified = true;
+                repoUser.Update(result);
+                status = true;
+                ViewBag.status = status;
+                ViewBag.message = "Hesabınız başarıyla aktive edilmiştir. Giriş yapabilirsiniz";
+
+            }
+            else
+            {
+                ViewBag.Status = status;
+                ViewBag.message = "Geçersiz istek";
+            }
+
+            return View("Login");
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login login,string ReturnUrl)
+        {
+            string message = "";
+            int sayac = 0;
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                user _user = repoUser.GetAll().Where(x => x.email == login.email).FirstOrDefault();
+                if (_user==null)
+                {
+                    message = "Böyle bir Email kayıtlı değil";
+                    ViewBag.message = message;
+                    ViewBag.status = status;
+                    return View();
+                }
+                // kullanıcı mail doğrulaması yapmışsa al, yapmamışsa false al.
+                bool verify = _user.isMailVerified ?? false;
+                if (!verify)
+                {
+                    message = " Mail doğrulaması yapılmamış";
+                    ViewBag.message = message;
+                    ViewBag.status = status;
+                    sayac++;
+                    _user.loginAttempt = sayac;
+                    repoUser.Update(_user);                   
+                }
+                // kullanıcı aktif değilse
+                if(_user.isActive==false)
+                {
+                    sayac++;
+                    message = "Hesabınız askıya alınmıştır";
+                    ViewBag.message = message;
+                    _user.loginAttempt = sayac;
+                    repoUser.Update(_user);
+                }
+                login.password = Crypto.Hash(login.password);
+                // şifreler tutuyorsa
+                if(string.Compare(login.password,_user.password)==0)
+                {
+                    _user.loginTime = DateTime.Now;
+                    _user.loginAttempt = sayac;
+                    repoUser.Update(_user);
+
+                    Session["username"] = _user.email;
+                    int timeOut = login.rememberMe ? 60 : 10;
+                    // form autentication oluşturalım.
+                    var ticket = new FormsAuthenticationTicket(login.email, login.rememberMe, timeOut);
+                    string encrypted = FormsAuthentication.Encrypt(ticket);
+                    var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted);
+                    cookie.Expires = DateTime.Now.AddMinutes(timeOut);
+                    cookie.HttpOnly = true;
+                    // Cookie ekleniyor
+                    FormsAuthentication.SetAuthCookie("userName", login.rememberMe);
+                    Response.Cookies.Add(cookie);
+
+                    // admin için
+                    if (_user.roleId == 1)
+                        return Redirect("~/Panel/Category");
+                    // return Url yerel bir url ise
+                    if (Url.IsLocalUrl(ReturnUrl))
+                    {
+                        return Redirect(ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Shop");
+                    }
+                }
+                else
+                {
+                    sayac++;
+                    _user.loginAttempt = sayac;
+                    repoUser.Update(_user);
+                    message = "Giriş yapılamadı.Parola yanlış!";
+                }
+
+            }
+            return View();
         }
 
         [NonAction]
