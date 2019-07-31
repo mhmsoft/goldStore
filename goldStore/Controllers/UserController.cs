ac21fe6f09e4bb6dd1aac34aff73f8e839c7a49b
Muhammet Ucar <mhmucar@hotmail.com>
2019-07-31 10:41:54 +0300

    register

---
+++ goldStore/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using goldStore.Areas.Panel.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace goldStore.Controllers
+{
+    public class UserController : Controller
+    {
+        CategoryRepository repoCategory = new CategoryRepository(new Areas.Panel.Models.goldstoreEntities());
+        BrandRepository repoBrand = new BrandRepository(new Areas.Panel.Models.goldstoreEntities());
+        // GET: User
+        public PartialViewResult PartialBrands()
+        {
+            // Tüm markaları partial View'e Gönder
+            return PartialView(repoBrand.GetAll());
+        }
+        public PartialViewResult PartialNewArrivals()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialPrice()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialCategory()
+        {
+            return PartialView(repoCategory.GetAll());
+        }
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
